b6d0e0d3ff3efb55a412778f3eedd825860b01ba
jackjk521 <jedchu541@gmail.com>
2022-10-08 13:29:21 +0800

    added bootstrap and product_dashboard
    Related Work Items: #125502

---
+++ mkstash/pages/admin/productdashboard.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function productDashboard() {
+    return
+        (
+           /* <div className="container">
+               {*//* <Head>
+
+                </Head>*//*}
+
+                <div>
+                    <h1>product dashboard</h1>
+                </div>
+
+            </div>*/
+            <div>
+                <h1>product dashboard</h1>
+            </div>
+        );
+}

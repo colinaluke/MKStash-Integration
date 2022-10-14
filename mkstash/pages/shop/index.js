61a6662655e5dc50efdf0c1fdae2d6ae940804d1
Carlina04 <cralinaamaba@gmail.com>
2022-10-14 09:20:38 +0800

    created navbar for shop page

--- mkstash/pages/shop/index.js
+++ mkstash/pages/shop/index.js
@@ -1,9 +1,23 @@
-import NavBar from './components/navbar';
+import NavBar from '../../components/shop/navbar';
+import React, { useState } from 'react'
+import styles from '../../styles/navbar.module.css'
 
-
-function Shop() {
+const Shop = () => {
+        const [visible, setVisible] = useState(true);
+        const removeElement = () => {
+            setVisible((prev) => !prev);
+        };
+ 
     return (
         <div>
+            <div>
+                {visible && (
+                    <div className={`text-center ${styles["top"]}`}>
+                            Lorem ipsum dolor sit BLACK FRIDAY SALE! 30% tempor incididunt
+                        <i onClick={removeElement} className={`bi bi-x-lg ${styles["xbutton"]}` }></i>
+                    </div>
+                )}
+            </div>
             <NavBar />
         </div>
         );

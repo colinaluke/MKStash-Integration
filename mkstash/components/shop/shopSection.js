95bbb33e124968356b9253dc86294c26998f724c
Carlina04 <cralinaamaba@gmail.com>
2022-10-17 14:34:29 +0800

    shop page

---
+++ mkstash/components/shop/shopSection.js
@@ -0,0 +1,14 @@
+import React, { Component, useState } from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from '../../styles/navbar.module.css'
+
+const ShopSection = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default ShopSection;

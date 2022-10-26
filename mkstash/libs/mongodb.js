90cb56d13dbd219e5891f81f2e952e60924bf759
samkayes <samanthasubaan@gmail.com>
2022-10-26 14:19:14 +0800

    added functionalities to the form

--- mkstash/libs/mongodb.js
+++ mkstash/libs/mongodb.js
@@ -1,36 +1,38 @@
+// libs/mongodb.js
+
 import { MongoClient } from "mongodb";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 // check the MongoDB URI
 if (!MONGODB_URI) {
-    throw new Error("Define the MONGODB_URI environmental variable");
+  throw new Error("Define the MONGODB_URI environmental variable");
 }
 
-let cachedClient = null;
+let cachedClient= null;
 let cachedDb = null;
 
 export async function connectToDatabase() {
-    // check the cached.
-    if (cachedClient && cachedDb) {
-        // load from cache
-        return {
-            client: cachedClient,
-            db: cachedDb,
-        };
-    }
-
-    // Connect to cluster
-    let client = new MongoClient(MONGODB_URI);
-    await client.connect();
-    let db = client.db("mkstash");
-
-    // set cache
-    cachedClient = client;
-    cachedDb = db;
-
+  // check the cached.
+  if (cachedClient && cachedDb) {
+    // load from cache
     return {
-        client: cachedClient,
-        db: cachedDb,
+      client: cachedClient,
+      db: cachedDb,
     };
+  }
+
+  // Connect to cluster
+  let client = new MongoClient(MONGODB_URI);
+  await client.connect();
+  let db = client.db("mkstash");
+
+  // set cache
+  cachedClient = client;
+  cachedDb = db;
+
+  return {
+    client: cachedClient,
+    db: cachedDb,
+  };
 }
---
+++ mkstash/pages/api/signup.js
@@ -0,0 +1,17 @@
+import { connectToDatabase } from "../../libs/mongodb";
+
+async function handler(req, res){
+
+    if(req.method !== 'POST') return
+
+    const {fname, lname, email, contactnum, gender, pass, cpass} = req.body;
+
+    const { db } = await connectToDatabase();
+    const collection = db.collection("users");
+    const result = await collection.insertOne({fname, lname, email, contactnum, gender, pass, cpass});
+
+    res.status(200).json({ message: "is invalid!"});
+
+}
+
+export default handler;

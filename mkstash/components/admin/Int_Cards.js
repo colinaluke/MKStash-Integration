917f85af4301ca77f06d96fb3b2a12818ff086e8
jackjk521 <jedchu541@gmail.com>
2022-10-29 13:03:39 +0800

    modal fixed with data rendering properly

--- mkstash/components/admin/Int_Cards.js
+++ mkstash/components/admin/Int_Cards.js
@@ -1,125 +1,28 @@
 
 import React, { useState, useMemo } from 'react';
-import 'antd/dist/antd.css';
-import { Modal } from 'antd';
-import { MdGpsFixed } from 'react-icons/md';
-import { BsPatchCheck } from 'react-icons/bs';
-import DL from '../../styles/AdminDashboard.module.css'
-
-import ModalPopUp from './ModalPopUp.js'
+import 'antd/dist/antd.css';        
+import CardModalPopUp from './ModalPopUp.js'
 import {ModalContext} from './ContextList.js'
 
 export default function interactive_Cards() {
     const [modalOpen, setModalOpen] = useState(false);
     const modalProviderValue = useMemo(() => ({ modalOpen, setModalOpen }), [modalOpen, setModalOpen]);
-/*    const providerValue = useMemo(() => ({ active, setActive }), [active, setActive]);*/
-    const [modalOrdersOpen, setModalOrdersOpen] = useState(false);
-    const [modalChargesOpen, setModalChargesOpen] = useState(false);
-    const [modalEarningsOpen, setModalEarningsOpen] = useState(false);
+
 
     return (
         <ModalContext.Provider value={modalProviderValue}>
             <div class="row w-100 m-0 p-0" id="changeTheme">
-                {/*<div class="col-lg-4 col-md-12 h-25 mt-0">
-                    <div class="card text-bg-primary my-3">
-                        <button type="button" class="btn btn-light w-100 h-100" onClick={() => setModalOpen(true)} id="changeTheme" data-status="active">
-                            <div class="row">
-                                <div class="col-8">
-                                    <div class="card-body text-start">
-                                        <h5 class="card-title text-dark"> ORDERS RECEIVED <MdGpsFixed /> </h5>
-                                        <p class="card-text-dark"> 5390 </p>
-                                    </div>
-                                </div>
-                                <div class="col-4 pt-3">
-                                    <BsPatchCheck size={56} />
-                                </div>
-                            </div>
-                        </button>
-                    </div>*/}
 
-                    {/* Modal for orders received card */}
+                    {/*ORDERS RECEIVED card*/}
+                    <CardModalPopUp title="ORDERS_RECEIVED" filter="ORDERS" />
 
-                    <ModalPopUp title="Orders Received" />
-                    {/*<Modal
-                        title="Orders Received"
-                        centered
-                        visible={modalOrdersOpen}
-                        onOk={() => setModalOrdersOpen(false)}
-                        onCancel={() => setModalOrdersOpen(false)}
-                    >
-                        <button class="btn btn-primary" onClick={() => window.location.replace("#graphs")}>
-                             Go to graphs 
-                        </button>
-                        <p>some contents...</p>
-                        <p>some contents...</p>
-                    </Modal>*/}
+                    {/*TOTAL CUSTOMERS card*/}
+                    <CardModalPopUp title="TOTAL_CUSTOMERS" />
 
-  {/*              </div>*/}
-                {/*end of orders card*/}
+                    {/*TOTAL EARNINGS card*/}
+                <CardModalPopUp title="TOTAL_EARNINGS" />
+               
 
-                <div class="col-lg-4 col-md-12 h-25">
-                    <div class="card text-bg-primary my-3">
-                        <button type="button" class="btn btn-light w-100 h-100" onClick={() => setModalChargesOpen(true)} id="changeTheme" data-status="active">
-                            <div class="row">
-                                <div class="col-8">
-                                    <div class="card-body text-start">
-                                        <h5 class="card-title text-dark">TOTAL CHARGES <MdGpsFixed /> </h5>
-                                        <p class="card-text-dark"> 2390 </p>
-                                    </div>
-                                </div>
-                                <div class="col-4 pt-3">
-                                    <BsPatchCheck size={56} />
-                                </div>
-                            </div>
-                        </button>
-                    </div>
-
-                    {/* Modal for total charges card */}
-
-                    <Modal
-                        title="Total Charges"
-                        centered
-                        visible={modalChargesOpen}
-                        onOk={() => setModalChargesOpen(false)}
-                        onCancel={() => setModalChargesOpen(false)}
-                    >
-                        <p>some contents...</p>
-                        <p>some contents...</p>
-                        <p>some contents...</p>
-                    </Modal>
-                </div>
-
-                <div class="col-lg-4 col-md-12 h-25">
-                    <div class="card text-bg-warning my-3">
-                        <button type="button" class="btn btn-light w-100 h-100" onClick={() => setModalEarningsOpen(true)} id="changeTheme" data-status="active">
-                            <div class="row">
-                                <div class="col-8">
-                                    <div class="card-body text-start">
-                                        <h5 class="card-title text-dark"> TOTAL EARNINGS <MdGpsFixed /> </h5>
-                                        <p class="card-text-dark"> 2390 </p>
-                                    </div>
-                                </div>
-                                <div class="col-4 pt-3">
-                                    <BsPatchCheck size={56} />
-                                </div>
-                            </div>
-                        </button>
-                    </div>
-
-                    {/* Modal for total earnings card */}
-
-                    <Modal
-                        title="Total Earnings"
-                        centered
-                        visible={modalEarningsOpen}
-                        onOk={() => setModalEarningsOpen(false)}
-                        onCancel={() => setModalEarningsOpen(false)}
-                    >
-                        <p>some contents...</p>
-                        <p>some contents...</p>
-                        <p>some contents...</p>
-                    </Modal>
-                </div>
             </div>
         </ModalContext.Provider>
         
--- mkstash/components/admin/ModalPopUp.js
+++ mkstash/components/admin/ModalPopUp.js
@@ -7,19 +7,22 @@ import { ModalContext } from './ContextList.js'
 import { MdGpsFixed } from 'react-icons/md';
 import { BsPatchCheck } from 'react-icons/bs';
 
-export default function ModalPopUp(title) {
+    
+
+export default function ModalPopUp({ title, filter }) {
 
     const { modalOpen, setModalOpen } = useContext(ModalContext);
+    const trueValue = true;
 
     return (
         <>
             <div class="col-lg-4 col-md-12 h-25 mt-0">
                 <div class="card text-bg-primary my-3">
-                    <button type="button" class="btn btn-light w-100 h-100" onClick={() => setModalOpen(true)} id="changeTheme" data-status="active">
+                    <button type="button" class="btn btn-light w-100 h-100" onClick={() => setModalOpen(trueValue)} id="changeTheme" data-status="active" data-bs-toggle="modal" data-bs-target={ `#${title}` }>
                         <div class="row">
                             <div class="col-8">
                                 <div class="card-body text-start">
-                                    <h5 class="card-title text-dark"> {title.title} <MdGpsFixed /> </h5>
+                                    <h5 class="card-title text-dark"> {title.replace('_', ' ')} <MdGpsFixed /> </h5>
                                     <p class="card-text-dark"> 5390 </p>
                                 </div>
                             </div>
@@ -29,20 +32,49 @@ export default function ModalPopUp(title) {
                         </div>
                     </button>
                 </div>
-            <Modal
-                title={title}
-                centered
-                open={modalOpen}
-                onOk={() => setModalOpen(false)}
-                onCancel={() => setModalOpen(false)}
-            >
-                <button class="btn btn-primary" onClick={() => window.location.replace("#ordersTable")}>
-                    Go to orders table
-                </button>
-                {/*{filter === null  }*/}
+{/*
+                <Modal
+                    title={ title }
+                    centered
+                    open={modalOpen}
+                    onOk={() => setModalOpen(false)}
+                    onCancel={() => setModalOpen(false)}
+                >
+                    {filter === "ORDERS" &&
+                        <button class="btn btn-primary" onClick={() => window.location.replace("#ordersTable")}>
+                        Go to orders table
+                        </button>
+                    }
+                 
+                */}{/*{filter === null  }*/}{/*
                 <p>some contents...</p>
                 <p>some contents...</p>
                 </Modal>
+*/}
+                <div class="modal fade" id={ title } tabindex="-3" aria-labelledby={ title } aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">{ title.replace('_', ' ') }</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                {filter === "ORDERS" &&
+                                    <button class="btn btn-primary" onClick={() => window.location.replace("#ordersTable")}>
+                                        Go to orders table
+                                    </button>
+                                }
+                                <p>some contents...</p>
+                                <p>some contents...</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </>
     )

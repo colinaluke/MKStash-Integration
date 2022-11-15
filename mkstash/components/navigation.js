d3911b77901238aa5fd28388b74c41904e77cfc2
samkayes <samanthasubaan@gmail.com>
2022-11-15 09:52:44 +0800

    added drowpdown menu overlays

--- mkstash/components/navigation.js
+++ mkstash/components/navigation.js
@@ -6,6 +6,7 @@ import React, {useState} from "react";
 const Navigation = () => {
 
     const [showShop, setShowShop] = useState(false);
+    const [showCollection, setShowCollection] = useState(false);
 
     return(
         <div className={styles.container}>
@@ -28,18 +29,65 @@ const Navigation = () => {
             <hr/>
             <div className={styles.item}>
                 <Link href="/shop"><p>Shop</p></Link><i class="bi bi-chevron-down" onClick={()=>setShowShop(true)}></i>
-                {showShop && (
+                
+                <p>Collection</p><i class="bi bi-chevron-down" onClick={()=>setShowCollection(true)}></i>
+                <Link href="/contact_us"><p>Contact Us</p></Link>
+            </div>
+            {showShop && (
                         <div className={styles.successOverlay}>
                            <div className={styles.innerOverlay}>
-                                <h2>Message Sent!</h2>
+                            <div className={styles.columns}>
+                                <ul>
+                                    <li><b>Men</b></li>
+                                    <li>T-shirt</li>
+                                    <li>Polo</li>
+                                    <li>Pants</li>
+                                    <li>Shorts</li>
+                                    <li>Hoodies</li>
+                                </ul>
+                                <ul>
+                                    <li><b>Women</b></li>
+                                    <li>T-shirt</li>
+                                    <li>Dress</li>
+                                    <li>Pants</li>
+                                    <li>Shorts</li>
+                                    <li>Hoodies</li>
+                                </ul>
+                                <ul>
+                                    <li><b>Accessories</b></li>
+                                    <li>Necklace</li>
+                                    <li>Bracelets</li>
+                                    <li>Earrings</li>
+                                </ul>
+                                <ul>
+                                    <li><b>Shoes</b></li>
+                                    <li>Sandals</li>
+                                    <li>Sneakers</li>
+                                </ul>
+                                <ul>
+                                    <li><b>Bags</b></li>
+                                    <li>Handbags</li>
+                                </ul>
+                                </div>
+                            </div>
+                        </div>
+                        )
+                }
+                {showCollection && (
+                        <div className={styles.successOverlay2}>
+                           <div className={styles.innerOverlay}>
+                                <ul>
+                                    <li>On Sale!</li>
+                                    <li>New Arrival</li>
+                                    <li>Best Seller</li>
+                                    <li>Branded</li>
+                                    <li>Surplus</li>
+                                </ul>
                                 <br/>
                             </div>
                         </div>
                         )
                 }
-                <p>Collection</p><i class="bi bi-chevron-down"></i>
-                <Link href="/contact_us"><p>Contact Us</p></Link>
-            </div>
         </div>
     );
 }
